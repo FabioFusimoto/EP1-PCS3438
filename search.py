62f4620cbba4b1541dcc78233b0cd0da0405e84c
Fábio Fusimoto Pires <fabiofusimoto.pires@hotmail.com>
2019-09-03 22:58:36 -0300

    Dia 1
    Progresso do dia 1 - Entendimento das funções já implementadas

--- search.py
+++ search.py
@@ -1,11 +1,12 @@
+# -*- coding: utf-8 -*-
 # search.py
 # ---------
 # Licensing Information:  You are free to use or extend these projects for
 # educational purposes provided that (1) you do not distribute or publish
 # solutions, (2) you retain this notice, and (3) you provide clear
 # attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
-# 
-# Attribution Information: The Pacman AI projects were developed at UC Berkeley.
+#
+# Attribution Information: The Pacman AI projects were developed at UC Berkeley
 # The core projects and autograders were primarily created by John DeNero
 # (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
 # Student side autograding was added by Brad Miller, Nick Hay, and
@@ -18,10 +19,11 @@ Pacman agents (in searchAgents.py).
 """
 
 import util
+# import sys
+
 
 class SearchProblem:
-    """
-    This class outlines the structure of a search problem, but doesn't implement
+    """This class outlines the structure of a search problem, but doesn't implement
     any of the methods (in object-oriented terminology: an abstract class).
 
     You do not need to change anything in this class, ever.
@@ -68,9 +70,11 @@ def tinyMazeSearch(problem):
     sequence of moves will be incorrect, so only use this for tinyMaze.
     """
     from game import Directions
+
     s = Directions.SOUTH
     w = Directions.WEST
-    return  [s, s, w, s, w, w, s, w]
+    return [s, s, w, s, w, w, s, w]
+
 
 def depthFirstSearch(problem):
     """
@@ -87,28 +91,43 @@ def depthFirstSearch(problem):
     print "Start's successors:", problem.getSuccessors(problem.getStartState())
     """
     "*** YOUR CODE HERE ***"
+    print("Start: ", problem.getStartState())
+    print("Is the start the goal: ",
+          problem.isGoalState(problem.getStartState()))
+    print("Start's successors: ",
+          problem.getSuccessors(problem.getStartState()))
     util.raiseNotDefined()
+
 
 def breadthFirstSearch(problem):
     """Search the shallowest nodes in the search tree first."""
     "*** YOUR CODE HERE ***"
     util.raiseNotDefined()
 
+
 def uniformCostSearch(problem):
     """Search the node of least total cost first."""
     "*** YOUR CODE HERE ***"
     util.raiseNotDefined()
 
+
 def nullHeuristic(state, problem=None):
     """
-    A heuristic function estimates the cost from the current state to the nearest
-    goal in the provided SearchProblem.  This heuristic is trivial.
+    A heuristic function estimates the cost from the current state to the
+    nearest goal in the provided SearchProblem.  This heuristic is trivial.
     """
     return 0
 
+
 def aStarSearch(problem, heuristic=nullHeuristic):
-    """Search the node that has the lowest combined cost and heuristic first."""
-    "*** YOUR CODE HERE ***"
+    """Search the node that has the lowest combined cost and heuristic first"""
+    from util import PriorityQueue
+    
+    print("Start position: " + str(problem.getStartState()))
+    print("Goal: " + str(problem.goal))
+    print("Heuristic for the start state: " + str(heuristic(
+          problem.getStartState(), problem)))
+
     util.raiseNotDefined()
 
 
